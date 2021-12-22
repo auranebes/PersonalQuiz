6758eb75b762f80e5ce80c238c4469f01ee31de4
Arslan Abdullaev <ars352@yahoo.com>
2021-12-22 22:32:13 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by brubru on 20.12.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var answers: [Answer]!
+    var types: AnimalType!
+    var array: [AnimalType]!
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результат в соответсвии с этим живтным
+    // 4. Избавится от кнопки возврата назад на экран результатов
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(answers!)
+        getResult()
+        
+    }
+
+
+}
+
+extension ResultsViewController {
+    private func getResult() {
+        //let dictCats: Dictionary<AnimalType, Int>
+        let typesOfAnswers = answers.map({$0.type})
+        print(typesOfAnswers)
+        //showCurrent()
+    }
+    
+}
+
